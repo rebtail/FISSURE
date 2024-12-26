d5d43b11e3960220c082fc8d2cb573816ab11598
Retail <115062310+rebtail@users.noreply.github.com>
2024-12-26 10:26:25 +0330

    Update 1_test_plugin_demod.py

--- Plugins/test_plugin/install_files/Flow Graph Library/maint-3.10/PD Flow Graphs/1_test_plugin_demod.py
+++ Plugins/test_plugin/install_files/Flow Graph Library/maint-3.10/PD Flow Graphs/1_test_plugin_demod.py
@@ -5,4 +5,4 @@ def print_message():
     print('Hello Plugin!')
 
 if __name__ == '__main__':
-    print_message()
+    print_message()#include <string>  char32_t UTF32_character = U'🔟'; // also written as U'\U0001F51F' std::u32string UTF32_string = U"UTF–32-encoded string"; // defined as `const char32_t*´
