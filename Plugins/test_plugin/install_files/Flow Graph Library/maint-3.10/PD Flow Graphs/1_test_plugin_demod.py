25ce5b1facd70c2f88e9876d997512c7e2c7c7bf
Retail <115062310+rebtail@users.noreply.github.com>
2024-12-26 10:27:29 +0330

    Update 1_test_plugin_demod.py

--- Plugins/test_plugin/install_files/Flow Graph Library/maint-3.10/PD Flow Graphs/1_test_plugin_demod.py
+++ Plugins/test_plugin/install_files/Flow Graph Library/maint-3.10/PD Flow Graphs/1_test_plugin_demod.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python3
+source_file.js:1
+<?xml version="1.0"?>
+^
+
+SyntaxError: Unexpected token <
+    at Module._compile (internal/modules/cjs/loader.js:723:23)
+    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)
+    at Module.load (internal/modules/cjs/loader.js:653:32)
+    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)
+    at Function.Module._load (internal/modules/cjs/loader.js:585:3)
+    at Function.Module.runMain (internal/modules/cjs/loader.js:831:12)
+    at startup (internal/bootstrap/node.js:283:19)
+    at bootstrapNodeJSCore (internal/bootstrap/node.js:622:3)
+Process finished with exit code 1.#!/usr/bin/python3
 # test file for install/uninstall
 
 def print_message():
